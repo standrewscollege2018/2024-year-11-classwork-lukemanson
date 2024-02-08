fc859491bc26b0b72cf1ad58128afc38b507d6ca
lukemanson <158853393+lukemanson@users.noreply.github.com>
2024-02-08 14:54:50 +1300

    A program that prints double the input.

---
+++ doublingprogram.py
@@ -0,0 +1,6 @@
+''' This program takes a number as input and doubles it! :')'''
+
+# Takes number as a float
+n = float(input("Please enter a number. This program will double it. "))
+# Print output
+print("The result was " + str(2*n) + ".")
