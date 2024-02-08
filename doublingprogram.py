7d85170a25beed321fa8038b8a6d1aa89ac3ad22
lukemanson <158853393+lukemanson@users.noreply.github.com>
2024-02-08 15:17:32 +1300

    Double the number, double the length of the program all for a useless cutscene!
    Description

--- doublingprogram.py
+++ doublingprogram.py
@@ -1,6 +1,33 @@
 ''' This program takes a number as input and doubles it! :')'''
-
-# Takes number as a float
-n = float(input("Please enter a number. This program will double it. "))
+import time
+keep_asking = True
+# Takes number as a float and asks again if the input is not a number
+while keep_asking:
+    try:
+        n = float(input("Please enter a number. This program will double it. "))
+        keep_asking = False
+    except ValueError:
+        time.sleep(1)
+        print("")
+        print("Please enter a number this time.")
+        time.sleep(2)
+        print("")
+print("")
+# Calculating Cutscene
+for i in range(5):
+    for i in range(20):
+        print("")
+    print("Calculating.")
+    time.sleep(0.5)
+    for i in range(20):
+        print("")
+    print("Calculating..")
+    time.sleep(0.5)
+    for i in range(20):
+        print("")
+    print("Calculating...")
+    time.sleep(0.5)
+for i in range(20):
+        print("")
 # Print output
 print("The result was " + str(2*n) + ".")
