b79e04bf03dbc62ba8f2461e3345b899e085dfff
lukemanson <158853393+lukemanson@users.noreply.github.com>
2024-02-29 15:21:31 +1300

    cars cars cars im tina form truneras!
    CARS CARS CRAS until 0!

--- cars cars cars!.py
+++ cars cars cars!.py
@@ -1,2 +1,27 @@
 # Format is [Car type, Seats, Availability]
-cars = [[Suzuki Van, 2, True], ]
+import time
+cars = [["Suzuki Van", 2, "", ""], ["Toyota Corolla", 4, "", ""], ["Honda CRV", 4, "", ""], ["Suzuki Swift", 4, "", ""], ["Mitsubishi Airtrek", 4, "", ""], ["Nissan DC Ute", 4, "", ""], ["Toyota Previa", 7, "", ""], ["Toyota Hi Ace", 12, "", ""], ["Toyota Hi Ace", 12, "", ""]]
+while True:
+    # Prints what vehicles exist
+    print("Vehicles:")
+    time.sleep(1)
+    for i in range(len(cars)):
+        time.sleep(0.5)
+        print(f"{i+1}. {cars[i][0]} ({cars[i][1]}) {cars[i][2]}")
+    time.sleep(1)
+    temp_user_name = input("What is your name? ")
+    temp_vehicle_booked = int(input("Which vehicle would you like to book? "))
+    no_car_picked = True
+    if temp_vehicle_booked == 0:
+        break
+    while no_car_picked:
+        if cars[temp_vehicle_booked - 1][2] == "Unavailable":
+            print("Please pick an available vehicle.")
+            time.sleep(1)
+            temp_vehicle_booked = int(input("Which vehicle would you like to book? "))
+        else:
+            print("Thank you for shopping with Briscoes.")
+            no_car_picked = False
+            cars[temp_vehicle_booked - 1][2] = "Unavailable"
+            cars[temp_vehicle_booked - 1][3] = temp_user_name
+
